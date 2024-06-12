103c70c311b4f970f18a4ea2aadd0b5e09ae6c0e
Courage <sdhoa1922@gmail.com>
2024-06-13 04:53:52 +0700

    config

--- src/configs/view-engine.js
+++ src/configs/view-engine.js
@@ -8,4 +8,5 @@ const { STATIC_FOLDER } = properties;
 export default function viewEngine(app) {
     if (!fs.existsSync(STATIC_FOLDER)) fs.mkdirSync(STATIC_FOLDER, { recursive: true });
     app.use(express.static(STATIC_FOLDER));
+    app.use(express.static('client'));
 };
