d110ed9eb83849bef5a6bb136aa7480ea8cf2eb2
sdhoa1922 <sdhoa1922@gmail.com>
2025-02-18 03:21:22 +0700

    commit

--- private/schema.js
+++ private/schema.js
@@ -14,8 +14,8 @@ export default {
             prd_image_path: "TEXT",
             prd_name: 'TEXT',
             prd_price: 'NUMERIC(10, 2)',
-            // prd_registered: "INTEGER DEFAULT (strftime('%s', CURRENT_TIMESTAMP))",
-            pr_cg_id: 'INTEGER REFERENCES CATEGORIES(cg_id) ON UPDATE CASCADE ON DELETE RESTRICT',
+            prd_registered: "INTEGER DEFAULT (strftime('%s', CURRENT_TIMESTAMP))",
+            pr_cg_id: 'INTEGER, FOREIGN KEY (pr_cg_id) REFERENCES CATEGORIES(cg_id) ON UPDATE CASCADE ON DELETE CASCADE',
         }
     }
 }
--- test/main.js
+++ test/main.js
@@ -1,7 +1,33 @@
 import { sql, utils, schema } from '../src/map.js';
 import AbstractDAO, { db } from '../src/@db/AbstractDAO.js'
+const prd = 'PRODUCTS', cg = 'CATEGORIES'
 
-db.all('PRAGMA foreign_key_check;', console.log)
-Object.keys(schema.tables).forEach(table =>
-    db.get(`PRAGMA foreign_key_list(${table});`, console.log)
-)
+// db.all('PRAGMA foreign_key_check;', console.log)
+// Object.keys(schema.tables).forEach(table =>
+//     db.get(`PRAGMA foreign_key_list(${table});`, console.log)
+// )
+
+// db.exec(sql.qDelete(cg, { cg_id: 2 }))
+// db.all(sql.qSelect(prd, '*', { pr_cg_id: [2] }), console.log)
+// db.all(sql.qSelect(cg, '*', { cg_id: 2 }), console.log)
+
+db.exec(`DROP TABLE IF EXISTS cha; DROP TABLE IF EXISTS con;`, console.log)
+
+db.exec('PRAGMA foreign_keys = ON;')
+db.run(`CREATE TABLE cha (
+    id INTEGER PRIMARY KEY,
+    ten TEXT
+);`)
+
+db.run(`
+    CREATE TABLE con (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_cha INTEGER,
+    ten TEXT,
+    FOREIGN KEY (id_cha) REFERENCES cha(id) ON DELETE CASCADE
+);`)
+await utils.promise(_ => 0,1e3)
+db.exec(`INSERT INTO cha(ten) VALUES ('Nguyễn Văn A'), ('Trần Thị B'); INSERT INTO con(id_cha, ten) VALUES (1, 'Con của A 1'),(1, 'Con của A 2'),(2, 'Con của B 1');`)
+db.exec(`DELETE FROM cha WHERE id = 1;`, console.log)
+db.each('SELECT * FROM cha', console.log)
+db.each('SELECT * FROM con', console.log)
